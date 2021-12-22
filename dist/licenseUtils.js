77f7bb31b6cdf28cd1ccd177fcfd04001e7a85c6
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2021-12-22 15:46:58 +0100

    🔖 Prepare release

--- dist/licenseUtils.js
+++ dist/licenseUtils.js
@@ -3,12 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.isValidLicense = isValidLicense;
-exports.licenseSnippets = void 0;
-const validLicense = ['MIT', 'BSD', 'Apache License', 'Creative Commons', 'GPL', 'Apache-2.0', 'BSD-3-Clause', "ISC"];
+exports.licenseSnippets = exports.isValidLicense = void 0;
+const validLicense = ['MIT', 'BSD', 'Apache License', 'Creative Commons', 'GPL', 'Apache-2.0', 'BSD-3-Clause', 'ISC'];
 const licenseSnippets = ['MIT License', 'The GNU General Public License is a free, copyleft license for software and other kinds of works.'];
 exports.licenseSnippets = licenseSnippets;
 
-function isValidLicense(license) {
-  return validLicense.some(i => i === license);
-}
+const isValidLicense = license => validLicense.some(i => i === license);
+
+exports.isValidLicense = isValidLicense;
